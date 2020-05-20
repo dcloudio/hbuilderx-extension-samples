de643f9be339096a4e2bee1db228cbd86c6e34a2
hx <hx@hxdeMac-mini.local>
2020-05-20 15:42:53 +0800

    snippets

--- api/window/setStatusBarMessage.js
+++ api/window/setStatusBarMessage.js
@@ -5,13 +5,13 @@ let hx = require('hbuilderx');
  * @param {String} level = ['info','error','warn']  - 消息级别
  */
 function setStatusBarMessage(level = 'info', hideAfterTimeOut = 0) {
-	let msg = "这是一条" + level + "级别的消息";
-	if (hideAfterTimeOut !== 0) {
-		msg = "这是一条能够自动隐藏的消息，" + String(hideAfterTimeOut) + "毫秒后消失。";
-	}
-	hx.window.setStatusBarMessage(msg,hideAfterTimeOut,level);
+  let msg = "这是一条" + level + "级别的消息 <a href='http://www.baidu.com'>查看url</a>";
+  if (hideAfterTimeOut !== 0) {
+    msg = "这是一条能够自动隐藏的消息，" + String(hideAfterTimeOut) + "毫秒后消失。";
+  }
+  hx.window.setStatusBarMessage(msg, hideAfterTimeOut, level);
 }
 
 module.exports = {
-	setStatusBarMessage
+  setStatusBarMessage
 };
--- api/window/showErrorMessage.js
+++ api/window/showErrorMessage.js
@@ -4,16 +4,18 @@ let hx = require('hbuilderx');
  * @description 窗口右下角错误通知框
  */
 function showErrorMessage() {
-	let resultPromise = hx.window.showErrorMessage('这是一个 error 相关的通知框。', ['我知道了','取消']);
-	resultPromise.then((result) => {
-		if (result === '我知道了') {
-			console.log('您点击的是: 我知道了');
-		} else if (result === '取消') {
-			console.log("您点击的是: 取消");
-		}
-	});
+  let resultPromise = hx.window.showErrorMessage('这是一个 error 相关的通知框。<a href="http://www.baidu.com">错误</a>', ['我知道了',
+    '取消'
+  ]);
+  resultPromise.then((result) => {
+    if (result === '我知道了') {
+      console.log('您点击的是: 我知道了');
+    } else if (result === '取消') {
+      console.log("您点击的是: 取消");
+    }
+  });
 }
 
 module.exports = {
-	showErrorMessage
+  showErrorMessage
 };
