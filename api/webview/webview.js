aa3ffcc47f6311667f18b4f9d286ab3fef1c1166
wdl_mac_mini <wandail@dcloud.io>
2020-08-21 14:53:38 +0800

    修改webview示例

--- api/webview/webview.js
+++ api/webview/webview.js
@@ -4,9 +4,10 @@ const hx = require('hbuilderx');
  * @description 显示webview
  */
 function showWebView(webviewPanel) {
+    hx.window.showErrorMessage('互相学习')
     let webview = webviewPanel.webView;
     
-    var background = '';
+    let background = '';
     
     let config = hx.workspace.getConfiguration();
     let colorScheme = config.get('editor.colorScheme');
@@ -23,24 +24,32 @@ function showWebView(webviewPanel) {
         <body style="background-color:${background};border:1px solid ${background};">
             <div style="max-width:200px;">
                 <img src="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-uni-mac-test/09318270-c742-11ea-b680-7980c8a877b8.gif" style="position: absolute;bottom: 0;left: 0;right: 0;width: 100%;margin: auto;">
+                <button onclick="send();">点我传送</button>
             </div>
             <script>
-                //    以下两种写法等同
-                hbuilderx.onDidReceiveMessage((msg) => {});
-                window.addEventListener("message", (msg) => {});
-                hbuiderx.postMessage({
-                    command: 'alert',
-                    text: 'HelloWorld'
-                });
+                function send() {
+                    hbuilderx.postMessage({
+                        command: 'alert',
+                        text: 'HelloWorld'
+                    });
+                }
+                
+                window.onload=function() {
+                    //    以下两种写法等同
+                    hbuilderx.onDidReceiveMessage((msg) => {
+                        console.log(msg)
+                    });
+                }
             </script>
         </body>
+        
       `;
-    webview.postMessage({
-        command: "test"
-    });
     webview.onDidReceiveMessage((msg) => {
         if (msg.command == 'alert') {
             hx.window.showInformationMessage(msg.text);
+            webview.postMessage({
+                command: "test"
+            });
         }
     });
 };
