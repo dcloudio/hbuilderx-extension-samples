3767025ff333f75aaf148c3e50ea8140e60f45b3
wdl_mac_mini <wandail@dcloud.io>
2020-11-27 15:18:39 +0800

    补充 api: hx.workspace.copyFileWithPrompt, code值说明

--- api/workspace/copyFileWithPrompt.js
+++ api/workspace/copyFileWithPrompt.js
@@ -1,12 +1,14 @@
 const hx = require('hbuilderx');
 
 /**
- * @description copyFileWithPrompt
+ * @description 把文件或目录，拷贝到指定目录下
+ * @param {String} src 绝对路径，目录或文件
+ * @param {String} dest 绝对路径，必须是目录
  */
-function copyFileWithPrompt() {
+function copyFileWithPrompt(src, dest) {
     hx.workspace.copyFileWithPrompt({
-        src: hx.Uri.file('F:/归档'),
-        dest: hx.Uri.file('F:/alpha/归档'),
+        src: hx.Uri.file(src),
+        dest: hx.Uri.file(dest),
         filter: function(params) {
             return 0;
         },
@@ -15,6 +17,17 @@ function copyFileWithPrompt() {
             return 0;
         }
     }).then((data) => {
+        // 0 操作成功
+        // 101  无效的路径/没有找到该文件对应的文件系统
+        // 102  没有找到该文件
+        // 103  打开文件失败
+        // 104  读取文件内容失败
+        // 105  写入文件内容失败
+        // 106  删除文件失败
+        // 107  创建文件夹失败
+        // 108  获取文件列表失败
+        // 109  创建临时文件夹失败
+        // 201  解析Json数据失败，可能是无效的Json数据
         console.log("--->", data);
     })
 };
