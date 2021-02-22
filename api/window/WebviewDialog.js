1e7a2f1b79167958d9392c0800b002e1a954ebdb
wdl <wandali@dcloud.io>
2021-02-22 15:15:46 +0800

    优化 webviewdialog示例

--- api/window/WebviewDialog.js
+++ api/window/WebviewDialog.js
@@ -5,9 +5,9 @@ const hx = require('hbuilderx');
  */
 function WebviewDialog() {
     let webviewDialog = hx.window.createWebViewDialog({
-        modal: false,
-        title: "Email校验",
-        description: "邮箱需符合“名称@域名”的规律",
+        modal: true,
+        title: "是否删除文件？",
+        description: "删除后无法恢复，请谨慎操作。也可以到回收站看看。",
         dialogButtons: [
             "确定", "取消"
         ],
@@ -18,36 +18,30 @@ function WebviewDialog() {
     }, {
         enableScripts: true
     });
-
-    let webview = webviewDialog.webView;
-    webview.html =`
-        <body>
-            <div>
-                <input type="email" style="border: 1px solid #bedbbb; border-radius: 5px; line-height: 30px; width: 100%;" placeholder="Enter email"/>
-            </div>
-        </body>
-        <script>
-            function initReceive() {
-                hbuilderx.onDidReceiveMessage((msg)=>{
-                    if(msg.type == 'DialogButtonEvent'){
-                        let button = msg.button;
-                        if(button == '确定'){
-                            //TODO 处理表单提交
-                        }else if(button == '取消'){
-                            //TODO 处理取消逻辑
-                             hbuilderx.postMessage({
-                                command: 'cancel'
-                            });
-                        }
-                    }
-                });
-            }
-            window.addEventListener("hbuilderxReady", initReceive);
-    `;
     
-    webview.postMessage({
-        command: "test"
-    });
+    let webview = webviewDialog.webView;
+    webview.html = `
+        <html>
+        <script>
+        function initReceive() {
+            hbuilderx.onDidReceiveMessage((msg)=>{
+                if(msg.type == 'DialogButtonEvent'){
+                    let button = msg.button;
+                    if(button == '确定'){
+                        //TODO 处理表单提交
+                    }else if(button == '取消'){
+                        //TODO 处理取消逻辑
+                            hbuilderx.postMessage({
+                            command: 'cancel'
+                        });
+                    }
+                }
+            });
+        }
+        window.addEventListener("hbuilderxReady", initReceive);
+        </script>
+        </html>
+    `;
     
     webview.onDidReceiveMessage((msg) => {
         console.log(msg)
@@ -56,7 +50,10 @@ function WebviewDialog() {
         }
     });
     
-    webviewDialog.show();
+    let promi = webviewDialog.show();
+    promi.then(function (data) {
+        // 处理错误信息
+    });
 };
 
 module.exports = WebviewDialog;
